24d1fc935a7379d75651fb35934d2b0dd528016c
Omar Jaroudi <ojaroudi@turo.com>
2023-08-17 20:47:58 +0000

    test: write actual test

--- index.test.js
+++ index.test.js
@@ -1,24 +1,47 @@
-const wait = require('./wait');
-const process = require('process');
-const cp = require('child_process');
-const path = require('path');
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+const { run } = require('./index'); // Update the path to match the actual location of index.js
 
-test('throws invalid number', async () => {
-  await expect(wait('foo')).rejects.toThrow('milliseconds not a number');
+jest.mock('@actions/core');
+jest.mock('@actions/exec');
+
+describe('Get Latest Commit Info Action', () => {
+  it('should set outputs for existing branch', async () => {
+    exec.exec.mockImplementation((command, args, options) => {
+      if (command === 'git ls-remote --exit-code --heads origin branch') {
+        return 0; // Simulate branch exists
+      }
+      if (command.startsWith('git log -1')) {
+        options.listeners.stdout('Test Commit Message\n');
+        return 0;
+      }
+    });
+
+    await run();
+
+    expect(core.setOutput).toHaveBeenCalledWith('commit-sha', expect.any(String));
+    expect(core.setOutput).toHaveBeenCalledWith('commit-message', 'Test Commit Message');
+    expect(core.setOutput).toHaveBeenCalledWith('commit-author', expect.any(String));
+  });
+
+  it('should set outputs for non-existing branch', async () => {
+    exec.exec.mockReturnValue(1); // Simulate branch doesn't exist
+
+    await run();
+
+    expect(core.setOutput).toHaveBeenCalledWith('commit-sha', '');
+    expect(core.setOutput).toHaveBeenCalledWith('commit-message', '');
+    expect(core.setOutput).toHaveBeenCalledWith('commit-author', '');
+  });
+
+  it('should handle errors', async () => {
+    const errorMessage = 'An error occurred';
+    exec.exec.mockImplementation(() => {
+      throw new Error(errorMessage);
+    });
+
+    await run();
+
+    expect(core.setFailed).toHaveBeenCalledWith(errorMessage);
+  });
 });
-
-test('wait 500 ms', async () => {
-  const start = new Date();
-  await wait(500);
-  const end = new Date();
-  var delta = Math.abs(end - start);
-  expect(delta).toBeGreaterThanOrEqual(500);
-});
-
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = 100;
-  const ip = path.join(__dirname, 'index.js');
-  const result = cp.execSync(`node ${ip}`, {env: process.env}).toString();
-  console.log(result);
-})
