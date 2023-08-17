5b3e60d7f6d02b75c96cefbbc17226a324038b13
Omar Jaroudi <ojaroudi@turo.com>
2023-08-17 20:37:41 +0000

    feat: create actual action logic

--- index.js
+++ index.js
@@ -1,21 +1,67 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const exec = require('@actions/exec');
 
+/**
+ * Check if branch name exists as remote ref
+ * @param {String} branch 
+ * @returns {Boolean} 
+ */
+async function checkBranchExists(branch) {
+  try {
+    await exec.exec(`git ls-remote --heads --exit-code --quiet origin ${branch}`);
+    return true;
+  } catch(e) {
+    if (e.message.includes('exit code 2')) {
+      return false;
+    }
+    throw e;
+  }
+}
 
-// most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const branch = core.getInput('branch-name');
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    const branchExists = await checkBranchExists(branch);
 
-    core.setOutput('time', new Date().toTimeString());
+    if (branchExists) {
+      // Get the latest commit SHA, message, and author
+      let latestCommitSHA = '';
+      let latestCommitMessage = '';
+      let latestCommitAuthor = '';
+      await exec.exec(`git fetch origin ${branch}`);
+      await exec.exec(`git log -1 --pretty=%H origin/${branch}`, [], {
+        listeners: {
+          stdout: (data) => {
+            latestCommitSHA += data.toString();
+          },
+        },
+      });
+      await exec.exec(`git log -1 --pretty=%B origin/${branch}`, [], {
+        listeners: {
+          stdout: (data) => {
+            latestCommitMessage += data.toString();
+          },
+        },
+      });
+      await exec.exec(`git log -1 --pretty=%an origin/${branch}`, [], {
+        listeners: {
+          stdout: (data) => {
+            latestCommitAuthor += data.toString();
+          },
+        },
+      });
+
+      core.setOutput('commit-sha', latestCommitSHA.trim());
+      core.setOutput('commit-message', latestCommitMessage.trim());
+      core.setOutput('commit-author', latestCommitAuthor.trim());
+    } else {
+      console.log(`Remote ref ${branch} not found`);
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
+module.exports = { run };
 run();
